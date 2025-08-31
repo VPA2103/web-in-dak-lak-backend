f5a3d8c1a035752908d7aec5e7067fd2327f8069
VPA2103 <150929603+VPA2103@users.noreply.github.com>
2025-08-31 20:07:42 +0700

    nhanh main moi

--- routes/upload.go
+++ routes/upload.go
@@ -9,4 +9,6 @@ import (
 func UploadRoutes(route *gin.Engine) {
 	route.POST("/upload", controllers.UploadHandler)
 	route.GET("/images", controllers.GetProductImage)
+	//route.GET("/images", controllers.GetProductImage)
+
 }
